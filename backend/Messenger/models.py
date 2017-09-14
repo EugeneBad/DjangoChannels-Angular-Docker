dd2221aab504473bf7df78474fd45f3fcc84bf83
eugenius <eudriku@gmail.com>
2017-09-14 15:53:38 +0300

    Add UserProfile model

--- backend/Messenger/models.py
+++ backend/Messenger/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    online_code = models.CharField(max_length=200, default='offline')
