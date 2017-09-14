e9a8bdd83727e7ee41a5c2db04c5f2589eb33777
eugenius <eudriku@gmail.com>
2017-09-14 17:20:00 +0300

    Add username password validation on login in login_rcv consumer

--- backend/Messenger/consumers.py
+++ backend/Messenger/consumers.py
@@ -19,7 +19,12 @@ def register_rcv(message):
 
             token = generate_token(credentials["username"])
 
-            message.reply_channel.send({"text": json.dumps({"status": 200, "token": token})})
+            message.reply_channel.send({"text": json.dumps({"status": 200,
+                                                            "token": token})
+                                        })
+
+    else:
+        message.reply_channel.send({"close": True})
 
 
 def login_conn(message):
@@ -27,7 +32,22 @@ def login_conn(message):
 
 
 def login_rcv(message):
-    pass
+    credentials = json.loads(message.content["text"])
+    if "username" in credentials and "password" in credentials:
+        user = User.objects.filter(username=credentials["username"]).first()
+
+        if user and user.check_password(credentials["password"]):
+            token = generate_token(credentials["username"])
+
+            message.reply_channel.send({"text": json.dumps({"status": 200,
+                                                            "token": token})
+                                        })
+
+        else:
+            message.reply_channel.send({"text": json.dumps({"status": "401"})})
+
+    else:
+        message.reply_channel.send({"close": True})
 
 
 def reject_conn(message):
