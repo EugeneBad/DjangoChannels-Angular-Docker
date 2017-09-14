873df9694ccf54605e53b0a79685c7f1261911d5
eugenius <eudriku@gmail.com>
2017-09-14 15:06:35 +0300

    Add login and register websocket routes

--- backend/djchannels/routing.py
+++ backend/djchannels/routing.py
@@ -1,5 +1,9 @@
 from channels import route
+from Messenger.consumers import register_conn, register_rcv, login_conn, login_rcv
 
 channel_routing = [
-    route()
+    route("websocket.connect", register_conn, path="/register"),
+    route("websocket.receive", register_rcv, path="/register"),
+    route("websocket.connect", login_conn, path="/login"),
+    route("websocket.receive", login_rcv, path="/login")
 ]
