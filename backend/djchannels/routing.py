b3cf2877ebc57aef60022a9541ba5a3f74caeb74
eugenius <eudriku@gmail.com>
2017-09-15 10:21:57 +0300

    Add route for fetching user messages

--- backend/djchannels/routing.py
+++ backend/djchannels/routing.py
@@ -1,5 +1,9 @@
 from channels import route
-from Messenger.consumers import register_conn, register_rcv, login_conn, login_rcv, reject_conn
+from Messenger.consumers import \
+    register_conn, register_rcv, \
+    login_conn, login_rcv, \
+    fetch_conn, \
+    reject_conn
 
 channel_routing = [
     route("websocket.connect", register_conn, path="/register"),
@@ -7,6 +11,9 @@ channel_routing = [
     route("websocket.connect", login_conn, path="/login"),
     route("websocket.receive", login_rcv, path="/login"),
 
+    route("websocket.connect", fetch_conn, path="/fetch/(?P<token>[\w]+)"),
 
+    
+    route("websocket.receive", reject_conn, path="/fetch/(?P<token>[\w]+)"),
     route("websocket.connect", reject_conn, path="/[\w]?")
 ]
