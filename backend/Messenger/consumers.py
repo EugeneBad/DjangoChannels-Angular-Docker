4f593fb40ae29a64c8db453db7ac4e15536f5b4e
eugenius <eudriku@gmail.com>
2017-09-15 11:28:26 +0300

    Create UserPrProfile object during register

--- backend/Messenger/consumers.py
+++ backend/Messenger/consumers.py
@@ -1,3 +1,5 @@
+from django.core.serializers import serialize
+from django.contrib.auth.models import User
 from . models import UserProfile, TextMessage
 import json
 from . utils import generate_token, is_authenticated
@@ -10,12 +12,12 @@ def register_conn(message):
 def register_rcv(message):
     credentials = json.loads(message.content["text"])
     if "username" in credentials and "password" in credentials:
-        if UserProfile.user.objects.filter(username=credentials["username"]).exists():
+        if User.objects.filter(username=credentials["username"]).exists():
             message.reply_channel.send({"text": json.dumps({"status": "409"})})
 
         else:
-            UserProfile.user.objects.create_user(username=credentials["username"],
-                                     password=credentials["password"])
+            UserProfile.objects.create(user=User.objects.create_user(credentials["username"],
+                                       password=credentials["password"]))
 
             token = generate_token(credentials["username"])
 
@@ -34,7 +36,7 @@ def login_conn(message):
 def login_rcv(message):
     credentials = json.loads(message.content["text"])
     if "username" in credentials and "password" in credentials:
-        user = UserProfile.user.objects.filter(username=credentials["username"]).first()
+        user = User.objects.filter(username=credentials["username"]).first()
 
         if user and user.check_password(credentials["password"]):
             token = generate_token(credentials["username"])
@@ -54,8 +56,8 @@ def fetch_users_conn(message, token):
     username = is_authenticated(token)
 
     if username:
-        current_user = UserProfile.user.objects.get(username=username)
-        
+        other_users = serialize('json', UserProfile.user.objects.exclude(username=username))
+        message.reply_channel.send({"text": other_users})
 
 
 def reject_conn(message):
