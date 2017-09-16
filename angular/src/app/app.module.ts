96e4ec24793bc736af1ad5cd7fe19bacbc558a0b
eugenius <eudriku@gmail.com>
2017-09-16 10:15:44 +0300

    Add dashboard component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthComponent
+    AuthComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
