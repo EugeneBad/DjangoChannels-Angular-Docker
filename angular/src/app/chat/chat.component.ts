fd35a2bc50b3f48941b4498cf8ba50a91f596fb1
eugenius <eudriku@gmail.com>
2017-09-18 09:30:31 +0300

    Import AfterViewChecked lifecycle hook to chat component

--- angular/src/app/chat/chat.component.ts
+++ angular/src/app/chat/chat.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewChecked {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+  }
+
+  ngAfterViewChecked(){
   }
 
 }
