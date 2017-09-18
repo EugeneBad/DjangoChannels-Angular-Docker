7a8a23d2ff003e2f311620d52771e85a4bf500f1
eugenius <eudriku@gmail.com>
2017-09-18 12:30:11 +0300

    Add socket to open and fetch users when dashboard component initialises

--- angular/src/app/dashboard/dashboard.component.ts
+++ angular/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { root_url } from '../url';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  token: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.token = window.sessionStorage.getItem('token');
+    
+    let userSocket = new WebSocket(root_url + "/fetch/users?" + this.token);
+
+    userSocket.onmessage = function(resp){
+      console.log(JSON.parse(resp.data));
+    }
+
   }
 
 }
