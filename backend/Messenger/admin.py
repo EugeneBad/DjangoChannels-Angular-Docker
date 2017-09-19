4c567b807b60db8d221bf6421a7e899a53de50df
eugenius <eudriku@gmail.com>
2017-09-19 11:24:11 +0300

    Register UsererProfile model

--- backend/Messenger/admin.py
+++ backend/Messenger/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from . models import TextMessage
+from . models import TextMessage, UserProfile
 
 admin.site.register(TextMessage)
+admin.site.register(UserProfile)
