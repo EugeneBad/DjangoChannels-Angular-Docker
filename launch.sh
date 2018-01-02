fcde50b764e56e3c7a637025efcf9debf8628759
eugenius <eudriku@gmail.com>
2018-01-02 13:52:19 +0300

    Add npm install to launch script

--- launch.sh
+++ launch.sh
@@ -1,1 +1,12 @@
-cd angular;ng build --prod;cd ..;docker-compose up -d; echo "App Launched Successfully!"
+#!/bin/bash
+# Install angular dependencies
+cd angular
+npm install
+
+#Build the javascript artifacts
+ng build --prod
+
+# Build and run docker containers
+cd ..
+docker-compose up -d
+echo "App Launched Successfully!"
