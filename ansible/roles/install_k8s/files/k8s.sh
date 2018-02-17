aafc794c10aa764a934a67583f5d0ee6185b8629
eugenius <eudriku@gmail.com>
2018-02-17 20:50:12 +0300

    Add k8s role

---
+++ ansible/roles/install_k8s/files/k8s.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
