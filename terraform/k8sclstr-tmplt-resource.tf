48be2c3b3630d8b204fc25f1d7f6d90e62430d72
eugenius <eudriku@gmail.com>
2018-04-29 04:55:40 +0300

    Reduce number of pods

--- terraform/k8sclstr-tmplt-resource.tf
+++ terraform/k8sclstr-tmplt-resource.tf
@@ -12,6 +12,6 @@ resource "google_compute_instance_template" "k8sclstr-template" {
     subnetwork = "${google_compute_subnetwork.private-subnetwork.name}"
   }
 
-  metadata_startup_script = "kubeadm join 10.0.0.2:6443 --token 81ylqd.rcjxmfjgnf6rfuxx --discovery-token-ca-cert-hash sha256:e539b3738e93bc09f5a86b90568c71580adc8148e97efead8cc732f416c46bca"
+  metadata_startup_script = "sudo kubeadm join 10.0.0.2:6443 --token 81ylqd.rcjxmfjgnf6rfuxx --discovery-token-ca-cert-hash sha256:e539b3738e93bc09f5a86b90568c71580adc8148e97efead8cc732f416c46bca"
 
 }
