517d115df487d07a47d5a2029d6a665347325986
eugenius <eudriku@gmail.com>
2018-04-30 10:27:30 +0300

    Apply nat routing to only db instance

--- terraform/routes-resource.tf
+++ terraform/routes-resource.tf
@@ -4,6 +4,6 @@ resource "google_compute_route" "private-to-nat-route" {
   network     = "${google_compute_network.djchangular-vpc.name}"
   next_hop_instance = "${google_compute_instance.k8smaster-nat-instance.name}"
   priority    = 100
-  tags = ["private"]
+  tags = ["db"]
   next_hop_instance_zone = "asia-south1-a"
 }
